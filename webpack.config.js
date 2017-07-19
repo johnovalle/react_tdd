4e75b552d039a6c2b96ac536b930d8a86acb840e
John Ovalle <johnovalle@gmail.com>
2017-07-20 01:25:46 +0900

    configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const buildDirectory = './dist';
+
+module.exports = {
+  entry: './lib/main.jsx',
+  devServer: {
+    hot:true,
+    inline true,
+    port: 8000,
+    historyApiFallback: true
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: path.resolve(buildDirectory),
+    filename: 'app.js',
+    publicPath: 'http://localhost:8000/dist'
+  },
+  externals: {
+    'cheerio': 'window',
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': true
+  },
+  module : {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-0'],
+      }
+    }]
+  },
+  plugins: []
+
+};
